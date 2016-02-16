621a2e019b7fb458467b27a12b0ae6e9774837a6
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2016-02-16 16:03:15 +0900

    Add functions of Connection

--- src/connection.js
+++ src/connection.js
@@ -5,4 +5,36 @@ export default class Connection {
     this.weight = 1;
     this.weightDiff = 0;
   }
+
+  setLeftUnit(unit) {
+    this.leftUnit = unit;
+  }
+
+  setRightUnit(unit) {
+    this.rightUnit = unit;
+  }
+
+  getLeftUnit() {
+    return this.leftUnit;
+  }
+
+  getRightUnit() {
+    return this.rightUnit;
+  }
+
+  setWeight(weight) {
+    this.weight = weight;
+  }
+
+  getWeight() {
+    return this.weight;
+  }
+
+  setWeightDiff(diff) {
+    this.weightDiff = diff;
+  }
+
+  getWeightDiff() {
+    return this.weightDiff;
+  }
 }
