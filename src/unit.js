8808c0be94f7f9270b1e83fd977c9ddac277b26a
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2016-02-16 13:39:05 +0900

    Add functions of Unit

--- src/unit.js
+++ src/unit.js
@@ -20,4 +20,88 @@ export default class Unit {
 
     this.delta = 0;
   }
+
+  getUnitType() {
+    return this.unitType;
+  }
+
+  setLeftConnections(connections) {
+    switch(this.unitType) {
+      case UnitType.HIDDEN, UnitType.OUTPUT:
+        throw new Error('Invalid unit type');
+      default:
+        this.leftConnections = connections;
+    }
+  }
+
+  setRightConnections(connections) {
+    switch(this.unitType) {
+      case UnitType.INPUT, UnitType.HIDDEN, UnitType.BIAS:
+        throw new Error('Invalid unit type');
+      default:
+        this.rightConnections = connections;
+    }
+  }
+
+  getLeftConnections() {
+    switch(this.unitType) {
+      case UnitType.HIDDEN, UnitType.OUTPUT:
+        throw new Error('Invalid unit type');
+      default:
+        return this.leftConnections;
+    }
+  }
+
+  getRightConnections() {
+    switch(this.unitType) {
+      case UnitType.INPUT, UnitType.HIDDEN, UnitType.BIAS:
+        throw new Error('Invalid unit type');
+      default:
+        return this.rightConnections;
+    }
+  }
+
+  setInput(value) {
+    switch(this.unitType) {
+      case UnitType.INPUT, UnitType.HIDDEN, UnitType.OUTPUT:
+        throw new Error('Invalid unit type');
+      default:
+        this.inputValue = value;
+    }
+  }
+
+  getInput() {
+    switch(this.unitType) {
+      case UnitType.INPUT, UnitType.HIDDEN, UnitType.OUTPUT:
+        throw new Error('Invalid unit type');
+      default:
+        return this.inputValue;
+    }
+  }
+
+  setOutput(value) {
+    this.outputValue = value;
+  }
+
+  getOutput() {
+    return this.outputValue;
+  }
+
+  setDelta(delta) {
+    switch(this.unitType) {
+      case UnitType.HIDDEn, UnitType.OUTPUT:
+        throw new Error('Invalid unit type');
+      default:
+        this.delta = delta;
+    }
+  }
+
+  getDelta() {
+    switch(this.unitType) {
+      case UnitType.HIDDEN, UnitType.OUTPUT:
+        throw new Error('Invalid unit type');
+      default:
+        return this.delta;
+    }
+  }
 }
