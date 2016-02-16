7ec5c079fbbbb1e6533e619ad7e87ed1ad300a8f
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2016-02-16 16:26:40 +0900

    Fix switch case in src/unit.js

--- src/unit.js
+++ src/unit.js
@@ -1,4 +1,4 @@
-const UnitType = {
+export const UnitType = {
   INPUT: 0,
   HIDDEN: 1,
   BIAS: 2,
@@ -27,55 +27,68 @@ export default class Unit {
 
   setLeftConnections(connections) {
     switch(this.unitType) {
-      case UnitType.HIDDEN, UnitType.OUTPUT:
-        throw new Error('Invalid unit type');
-      default:
+      case UnitType.HIDDEN:
+      case  UnitType.OUTPUT:
         this.leftConnections = connections;
+        break;
+      default:
+        throw new Error('Invalid unit type');
     }
   }
 
   setRightConnections(connections) {
     switch(this.unitType) {
-      case UnitType.INPUT, UnitType.HIDDEN, UnitType.BIAS:
-        throw new Error('Invalid unit type');
-      default:
+      case UnitType.INPUT:
+      case UnitType.HIDDEN:
+      case UnitType.BIAS:
         this.rightConnections = connections;
+        break;
+      default:
+        throw new Error('Invalid unit type');
     }
   }
 
   getLeftConnections() {
     switch(this.unitType) {
-      case UnitType.HIDDEN, UnitType.OUTPUT:
-        throw new Error('Invalid unit type');
-      default:
+      case UnitType.HIDDEN:
+      case UnitType.OUTPUT:
         return this.leftConnections;
+      default:
+        throw new Error('Invalid unit type');
     }
   }
 
   getRightConnections() {
     switch(this.unitType) {
-      case UnitType.INPUT, UnitType.HIDDEN, UnitType.BIAS:
-        throw new Error('Invalid unit type');
-      default:
+      case UnitType.INPUT:
+      case UnitType.HIDDEN:
+      case UnitType.BIAS:
         return this.rightConnections;
+      default:
+        throw new Error('Invalid unit type');
     }
   }
 
   setInput(value) {
     switch(this.unitType) {
-      case UnitType.INPUT, UnitType.HIDDEN, UnitType.OUTPUT:
-        throw new Error('Invalid unit type');
-      default:
+      case UnitType.INPUT:
+      case UnitType.HIDDEN:
+      case UnitType.OUTPUT:
         this.inputValue = value;
+        break;
+      default:
+        throw new Error('Invalid unit type');
     }
   }
 
   getInput() {
     switch(this.unitType) {
-      case UnitType.INPUT, UnitType.HIDDEN, UnitType.OUTPUT:
-        throw new Error('Invalid unit type');
-      default:
+      case UnitType.INPUT:
+      case UnitType.HIDDEN:
+      case UnitType.OUTPUT:
         return this.inputValue;
+      default:
+        throw new Error('Invalid unit type');
     }
   }
 
@@ -89,19 +102,22 @@ export default class Unit {
 
   setDelta(delta) {
     switch(this.unitType) {
-      case UnitType.HIDDEn, UnitType.OUTPUT:
-        throw new Error('Invalid unit type');
-      default:
+      case UnitType.HIDDEN:
+      case UnitType.OUTPUT:
         this.delta = delta;
+        break;
+      default:
+        throw new Error('Invalid unit type');
     }
   }
 
   getDelta() {
     switch(this.unitType) {
-      case UnitType.HIDDEN, UnitType.OUTPUT:
-        throw new Error('Invalid unit type');
-      default:
+      case UnitType.HIDDEN:
+      case UnitType.OUTPUT:
         return this.delta;
+      default:
+        throw new Error('Invalid unit type');
     }
   }
 }
