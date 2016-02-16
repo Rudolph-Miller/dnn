81e77f7661d2ce0de90fceceb9a0b7102f876b80
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2016-02-16 13:23:02 +0900

    Add getMeanAndSD

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-import { rnorm, randomChoice } from './util';
+import { rnorm, randomChoice, getMeanAndSD } from './util';
 
 console.log(rnorm(10, 5));
 console.log(randomChoice([1, 2, 3], 2));
+console.log(getMeanAndSD([
+  { data: [1, 2, 3] },
+  { data: [2, 3, 4] },
+  { data: [3, 4, 5] }
+]));
--- src/util.js
+++ src/util.js
@@ -25,3 +25,41 @@ export function randomChoice(ary, count) {
   }
   return newAry;
 }
+
+export function getMeanAndSD(dataSet) {
+  let sum = [];
+  for (let i = 0; i < dataSet.length; i++) {
+    const items = dataSet[i]['data'];
+    for (let j = 0; j < items.length; j++) {
+      if (sum[j] === undefined) {
+        sum[j] = 0;
+      }
+      sum[j] += items[j];
+    }
+  }
+
+  let means = [];
+  for (let k = 0; k < sum.length; k++) {
+    means[k] = sum[k] / dataSet.length;
+  }
+
+  let squaredSum = [];
+  for (let l = 0; l < dataSet.length; l++) {
+    const items2 = dataSet[l]['data'];
+    for (let m = 0; m < items2.length; m++) {
+      if (squaredSum[m] === undefined) {
+        squaredSum[m] = 0;
+      }
+      squaredSum[m] += Math.pow((items2[m] - means[m]), 2);
+    }
+  }
+
+  let sds = [];
+  for (let p = 0; p < dataSet.length; p++) {
+    const items3 = dataSet[p]['data'];
+    for (let q = 0; q < items3.length; q++) {
+      sds[q] = Math.sqrt(squaredSum[q] / dataSet.length);
+    }
+  }
+  return {means: means, sds: sds};
+}
